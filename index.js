1c45c11e8496685427965c18bbe13585479854de
phuvu <phu270904@gmail.com>
2024-09-23 21:07:38 +0700

    base auth route

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import connectDB from './mongodb/connect.js';
 import postRoutes from './routes/postRoutes.js';
 import generateRoutes from './routes/generateRoutes.js';
+import authRoutes from './routes/authRoutes.js';
 
 dotenv.config();
 
@@ -11,6 +12,7 @@ const app = express();
 app.use(cors());
 app.use(express.json({ limit: '50mb' }));
 
+app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/posts', postRoutes);
 app.use('/api/v1/generate', generateRoutes);
 
---
+++ routes/authRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
+const router = express.Router();
+
+router.route('/').get((req, res) => {
+    res.send("Hello from Auth AI-Genage");
+})
+
+export default router;
